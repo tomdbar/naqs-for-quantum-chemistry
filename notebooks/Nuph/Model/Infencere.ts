164a52169b1572c73747d0657cc8615c3b9dbb83
L <91376574+LeoTzahal18@users.noreply.github.com>
2021-10-19 16:14:59 -0300

    Create Infencere.ts

---
+++ notebooks/Nuph/Model/Infencere.ts
@@ -0,0 +1,43 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+ {InferenceSession, OnnxValue}    'onnxruntime-common';
+
+     SessionOptions = InferenceSession.SessionOptions;
+     FeedsType = {
+  [name: string]: OnnxValue;
+};
+     FetchesType = {
+  [name: string]: OnnxValue;
+};
+     ReturnType = {
+  [name: string]: OnnxValue;
+};
+     RunOptions = InferenceSession;
+
+
+/**
+ * Binding exports a simple synchronized inference session object wrap.
+ */
+                      Binding {
+                    InferenceSession {
+             (modelPath: string, options: SessionOptions):    ;
+    loadModel(buffer: ArrayBuffer, byteOffset: number, byteLength: number, options: SessionOptions):    ;
+
+             inputNames: string[];
+             outputNames: string[];
+
+    run(feeds: FeedsType, fetches: FetchesType, options: RunOptions): ReturnType;
+  }
+
+                   InferenceSessionConstructor {
+       (): InferenceSession;
+  }
+}
+
+// export 
+             binding =
+    // eslint-disable-next-line typescript-eslint/no-require-imports, typescript-eslint/no-var-requires
+            (`../bin/napi-v3/${process.platform}/${process.arch}/onnxruntime_binding.node`) 
+    // eslint-disable-next-line typescript-eslint/naming-convention
+    {InferenceSession: Binding.InferenceSessionConstructor};
